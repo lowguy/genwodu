2fd5f124a18307f2770fb45a871cfb66bd5f7324
890 <810093817@qq.com>
2017-05-03 18:40:38 +0800

    mac

---
+++ pages/blog/index.js
@@ -0,0 +1,36 @@
+// pages/blog/index.js
+var WxParse = require('../../wxParse/wxParse.js');
+Page({
+  data:{
+    apiUrl:'https://890vip.cn',
+    blog:[]
+  },
+  onLoad:function(options){
+    var that = this
+    wx.request({
+        url: 'https://890vip.cn/vip/index/lists',
+        header: {
+            'content-type': 'application/json'
+        },
+        success: function(res) {
+          console.log('blog',res.data.result)
+          that.setData({
+            blog:res.data.result
+          })
+        }
+      })
+
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
