c8d0cdb2b511892dcd7c820520fc2a59647ebd7d
890 <810093817@qq.com>
2017-05-12 10:00:02 +0800

    下拉加载

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,28 +1,98 @@
 // pages/blog/index.js
-var WxParse = require('../../wxParse/wxParse.js');
+
 Page({
   data:{
     apiUrl:'https://890vip.cn',
+    scrollTop: 100,
+    scrollHeight:0,
+    hidden:true,
+    refresh:false,
+    hasmore:false,
+    shareTitle:'blogs.890vip.cn',
+    shareId:0,
+    page:1,
     blog:[]
   },
-  caidan:function(e){
-    console.log(890)
-    
-  },
-  onLoad:function(options){
+  onShareAppMessage: function () {
     var that = this
+    return {
+      title: that.data.shareTitle,
+      path: '/detail/id/'+that.data.shareId,
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
+  },
+  menus:function(e){
+    var that = this
+    that.setData({
+        shareTitle:e.currentTarget.dateset.title,
+        shareId:e.currentTarget.dateset.id
+    });
+  },
+  //加载状态
+  loading:function(hidden){
+    var that = this
+    that.setData({
+        hidden:hidden,
+        refresh:!hidden
+    });
+  },
+  //滑动到顶部
+  upper: function(e) {
+    console.log(e)
+  },
+  //滑动到底部
+  lower: function(e) {
+    var that = this
+    if(that.data.refresh||that.data.hasmore){
+      return 
+    }
+    var page = that.data.page
+    page++
+    that.setData({
+       page:page
+    })
+    that.getData(that.data.page)
+  },
+  //加载数据
+  getData:function(page){
+    var that = this
+    that.loading(false)
     wx.request({
-        url: 'https://890vip.cn/vip/index/lists',
+        url: `https://890vip.cn/vip/index/lists`,
         header: {
             'content-type': 'application/json'
         },
+        method:'post',
+        data:{page:page},
         success: function(res) {
+          console.log(res.data.result.length);
+          if(!res.data.result.length){
+              that.setData({
+                  hasmore:true
+              })
+          }
           that.setData({
-            blog:res.data.result
+            blog:that.data.blog.concat(res.data.result)
           })
+          that.loading(true);
         }
-      })
-
+      });
+  },
+  onLoad:function(options){
+    var that = this
+    wx.getSystemInfo({
+        success:function(res){
+            that.setData({
+                scrollHeight:res.windowHeight
+            });
+        }
+    });
+    that.getData(that.data.page)
   },
   onReady:function(){
     // 页面渲染完成
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,33 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-    primary: '搜索',
-    search_palceholder:'标题/作者',
-    userInfo: {},
-    apiUrl:'https://890vip.cn',
-    recommend:[]
+    toView: 'red',
+    scrollTop: 100
   },
- 
-  onLoad: function () {
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+  tap: function(e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function(e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   }
 })
